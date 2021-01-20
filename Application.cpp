5a06ee71bd668f20895752566c0c150410bab2d4
Oleksii Serdiukov <oleksii.serdiukov@nordwhale.com>
2021-01-20 03:02:17 +0200

    refactored app to use a signtelon, reimplemented run()

--- Application.cpp
+++ Application.cpp
@@ -4,22 +4,81 @@
 
 #include "Application.h"
 
-void Application::run() {
-  RenderWindow window(VideoMode(200, 200), "SFML works!");
-  CircleShape shape(100.f);
-  shape.setFillColor(Color::Green);
+Application* Application::instance = nullptr;
 
+Application* Application::create(string resourcePath) {
+  if (instance == nullptr) {
+    instance = new Application(resourcePath);
+  }
+  
+  return instance;
+}
+
+int Application::run() {
+  // Create the main window
+  RenderWindow window(VideoMode(800, 600), "SFML window");
+
+  // Set the Icon
+  Image icon;
+  if (!icon.loadFromFile(resourcePath + "icon.png")) {
+      return EXIT_FAILURE;
+  }
+  window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+  // Load a sprite to display
+  Texture texture;
+  if (!texture.loadFromFile(resourcePath + "cute_image.jpg")) {
+      return EXIT_FAILURE;
+  }
+  Sprite sprite(texture);
+
+  // Create a graphical text to display
+  Font font;
+  if (!font.loadFromFile(resourcePath + "sansation.ttf")) {
+      return EXIT_FAILURE;
+  }
+  Text text("Hello SFML", font, 50);
+  text.setFillColor(Color::Black);
+
+  // Load a music to play
+  Music music;
+  if (!music.openFromFile(resourcePath + "nice_music.ogg")) {
+      return EXIT_FAILURE;
+  }
+
+  // Play the music
+  music.play();
+
+  // Start the game loop
   while (window.isOpen())
   {
-    Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == Event::Closed)
-        window.close();
-    }
+      // Process events
+      Event event;
+      while (window.pollEvent(event))
+      {
+          // Close window: exit
+          if (event.type == Event::Closed) {
+              window.close();
+          }
 
-    window.clear();
-    window.draw(shape);
-    window.display();
+          // Escape pressed: exit
+          if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+              window.close();
+          }
+      }
+
+      // Clear screen
+      window.clear();
+
+      // Draw the sprite
+      window.draw(sprite);
+
+      // Draw the string
+      window.draw(text);
+
+      // Update the window
+      window.display();
   }
+
+  return EXIT_SUCCESS;
 }
--- Application.h
+++ Application.h
@@ -5,13 +5,26 @@
 #ifndef MEOWMEOW_APPLICATION_H
 #define MEOWMEOW_APPLICATION_H
 
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+using namespace std;
 
 class Application {
+  
+  protected:
+    Application(string withResourcePath) : resourcePath(withResourcePath) {}
+  
+    static Application* instance;
+    string resourcePath;
+    
   public:
-    static void run();
+    Application(Application &other) = delete;
+    void operator=(const Application &) = delete;
+  
+    static Application *create(string resourcePath);
+    int run();
 };
 
 
